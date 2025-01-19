c379ad15b8c479394d62704d6070e511aafe934a
pokhrelgopal <pokhrelgopal27@gmail.com>
2025-01-19 12:52:25 +0545

    refactor user routes to use controller namespace for improved readability

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,31 +1,19 @@
 import express from "express";
-import {
-  register,
-  login,
-  logout,
-  deleteUser,
-  forgotPassword,
-  getUserById,
-  getUsers,
-  setNewPassword,
-  updateUser,
-  getMe,
-  verifyUser,
-} from "../controller/user.controller";
+import * as uc from "../controller/user.controller";
 import { authMiddleware, permissionMiddleware } from "../middleware.ts";
 
 const router = express.Router();
 
-router.post("/register", register);
-router.post("/login", login);
-router.post("/logout", authMiddleware, logout);
-router.post("/forgot-password", forgotPassword);
-router.get("/me", authMiddleware, getMe);
-router.post("/set-new-password", setNewPassword);
-router.post("/verify", verifyUser);
-router.get("/", authMiddleware, getUsers);
-router.get("/:id", [authMiddleware, permissionMiddleware], getUserById);
-router.put("/:id", [authMiddleware, permissionMiddleware], updateUser);
-router.delete("/:id", [authMiddleware, permissionMiddleware], deleteUser);
+router.post("/register", uc.register);
+router.post("/login", uc.login);
+router.post("/logout", authMiddleware, uc.logout);
+router.post("/forgot-password", uc.forgotPassword);
+router.get("/me", authMiddleware, uc.getMe);
+router.post("/set-new-password", uc.setNewPassword);
+router.post("/verify", uc.verifyUser);
+router.get("/", authMiddleware, uc.getUsers);
+router.get("/:id", [authMiddleware, permissionMiddleware], uc.getUserById);
+router.put("/:id", [authMiddleware, permissionMiddleware], uc.updateUser);
+router.delete("/:id", [authMiddleware, permissionMiddleware], uc.deleteUser);
 
 export default router;
